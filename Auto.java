5beee0218cbf5b1c17e5b6af09a044d2d22ae2ca
Raman Haji <126676583+Raman74@users.noreply.github.com>
2025-12-19 07:59:16 +0000

    Auto BMW

---
+++ Auto.java
@@ -0,0 +1,15 @@
+public class Auto {
+    
+    public String marke;
+    public String farbe;
+    public String kennzeichen;
+
+    public void Auto(String pMarke, String pFarbe, String pKennzeichen){
+        marke = pMarke;
+        farbe = pFarbe;
+        kennzeichen = pKennzeichen;
+    }
+
+
+    
+}
--- ProgrammStart.java
+++ ProgrammStart.java
@@ -1,8 +1,26 @@
 public class ProgrammStart {
     public static void main(String[] args) {
         System.out.println("Prakhaus Programm");
-
         
+        Auto meinbmw = new Auto();
+        meinbmw.farbe = "Schwarz";
+        meinbmw.marke = "BMW";
+        meinbmw.kennzeichen = "K-KK 123";
+        Auto [] stellplaetze = new Auto[100];
+
+        for(int i=0; i<100;i++){
+            System.out.println("Stellplatz:" + (i+1) + ":" + stellplaetze[i]);
+
+        }
+        stellplaetze[0] = meinbmw;
+
+        for(int i=0; i<100;i++){
+            System.out.println("Stellplatz:" + (i+1) + ":" + stellplaetze[i]);
+
+        }
+
+
+
         
     }
 }
